778ae2152dbf146c94050d674235ed0adfdaae37
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-15 16:35:21 +0100

    git commit -m "SFMS"

--- src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassService.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassService.java
@@ -1,4 +1,15 @@
 package com.sfms.schoolfilemanagementsystem.services;
 
+import com.sfms.schoolfilemanagementsystem.services.ClassRegistrationDto;
+import com.sfms.schoolfilemanagementsystem.model.Class;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface ClassService {
+
+    Class registerWith(ClassRegistrationDto classRegistrationDto);
+    Optional<Class> findByClassId(Long classId);
+    void deleteClassBy(Long classId);
+    List<Class> findAll();
 }
