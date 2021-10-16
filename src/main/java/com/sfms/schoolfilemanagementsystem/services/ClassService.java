45d522c11d84d0a020457de46b226d0089b7d524
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-16 21:06:17 +0100

    git commit -m "SFMS"

--- src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassService.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassService.java
@@ -1,10 +1,13 @@
 package com.sfms.schoolfilemanagementsystem.services;
 
+import com.sfms.schoolfilemanagementsystem.model.Student;
+import com.sfms.schoolfilemanagementsystem.model.Subject;
 import com.sfms.schoolfilemanagementsystem.services.ClassRegistrationDto;
 import com.sfms.schoolfilemanagementsystem.model.Class;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface ClassService {
 
@@ -12,5 +15,6 @@ public interface ClassService {
     Optional<Class> findByClassId(Long classId);
     void deleteClassBy(Long classId);
     List<Class> findAll();
-
+    List<Student> findAllStudentsInThisClassWith(Long classId);
+    List<Subject> findAllSubjectsForThisClassWith(Long classId);
 }
--- src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassServiceImpl.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/services/ClassServiceImpl.java
@@ -3,22 +3,31 @@ package com.sfms.schoolfilemanagementsystem.services;
 import com.sfms.schoolfilemanagementsystem.dto.StudentRegistrationDto;
 import com.sfms.schoolfilemanagementsystem.model.Class;
 import com.sfms.schoolfilemanagementsystem.model.Student;
+import com.sfms.schoolfilemanagementsystem.model.Subject;
 import com.sfms.schoolfilemanagementsystem.repository.ClassRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ClassServiceImpl implements ClassService{
 
     @Autowired
     ClassRepository classRepository;
-    Student student1 = new Student();
+
+    Class class1 = new Class();
+
+    @Autowired
+    StudentServices studentServices;
+
+    @Autowired
+    SubjectServices subjectServices;
 
     @Override
     public Class registerWith(ClassRegistrationDto classRegistrationDto) {
-        Class class1 = new Class();
         class1.setClassName(classRegistrationDto.getClassName());
         class1.setClassType(classRegistrationDto.getClassType());
         return classRepository.save(class1);
@@ -37,6 +46,27 @@ public class ClassServiceImpl implements ClassService{
     @Override
     public List<Class> findAll() {
         return classRepository.findAll();
+    }
+
+    @Override
+    public List<Student> findAllStudentsInThisClassWith(Long classId) {
+//        class1.getClassId().equals(classId);
+//        return class1.getStudents();
+        Optional<Class> optionalClass = classRepository.findById(classId);
+        if(optionalClass.isPresent()){
+            return (List<Student>) optionalClass.get().getStudents();
+        }
+        return null;
+    }
+
+    @Override
+    public List<Subject> findAllSubjectsForThisClassWith(Long classId) {
+        return subjectServices.findAll().stream().filter(subject -> {
+            for(Class class1 : subject.getClasses()){
+                return class1.getClassId().equals(classId);
+            }
+            return false;
+        }).collect(Collectors.toList());
     }
 
 
