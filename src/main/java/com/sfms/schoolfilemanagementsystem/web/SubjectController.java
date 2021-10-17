7313f1c43a4ff4193d144a66ba8ec7f8206b4cf0
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-17 16:34:35 +0100

    git commit -m "SFMS"

--- src/main/java/com/sfms/schoolfilemanagementsystem/web/SubjectController.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/web/SubjectController.java
@@ -1,12 +1,14 @@
 package com.sfms.schoolfilemanagementsystem.web;
 
+import com.sfms.schoolfilemanagementsystem.model.Student;
 import com.sfms.schoolfilemanagementsystem.model.Subject;
 import com.sfms.schoolfilemanagementsystem.services.SubjectRegistrationDto;
 import com.sfms.schoolfilemanagementsystem.services.SubjectServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SubjectController {
@@ -18,6 +20,22 @@ public class SubjectController {
     public Subject registerWith(@RequestBody SubjectRegistrationDto subjectRegistrationDto){
         return subjectServices.registerWith(subjectRegistrationDto);
     }
+
+    @GetMapping("subject/findAll")
+    public List<Subject> findAll(){
+        return subjectServices.findAll();
+    }
+
+    @GetMapping("subject/findby{subjectId}")
+    public Optional<Subject> findBySubjectId(@PathVariable Long subjectId){
+        return subjectServices.findBySubjectId(subjectId);
+    }
+
+    @DeleteMapping("subject/delete/{subjectId}")
+    public void deleteSubjectBy(@PathVariable Long subjectId){
+        subjectServices.deleteSubjectBy(subjectId);
+    }
+
 
 
 }
