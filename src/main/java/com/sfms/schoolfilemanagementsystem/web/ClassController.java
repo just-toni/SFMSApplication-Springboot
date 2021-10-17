528732c85c5c6b959ceb78abb98898bff463dd93
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-17 16:34:04 +0100

    git commit -m "SFMS"

--- src/main/java/com/sfms/schoolfilemanagementsystem/web/ClassController.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/web/ClassController.java
@@ -1,8 +1,15 @@
 package com.sfms.schoolfilemanagementsystem.web;
 
+import com.sfms.schoolfilemanagementsystem.model.Class;
+import com.sfms.schoolfilemanagementsystem.model.Subject;
+import com.sfms.schoolfilemanagementsystem.services.ClassRegistrationDto;
 import com.sfms.schoolfilemanagementsystem.services.ClassService;
+import com.sfms.schoolfilemanagementsystem.services.SubjectRegistrationDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class ClassController {
@@ -10,5 +17,23 @@ public class ClassController {
     @Autowired
     private ClassService classService;
 
+    @PostMapping("class/add")
+    public Class registerWith(@RequestBody ClassRegistrationDto classRegistrationDto){
+        return classService.registerWith(classRegistrationDto);
+    }
 
+    @GetMapping("class/findAll")
+    public List<Class> findAll(){
+        return classService.findAll();
+    }
+
+    @GetMapping("class/findBy{classId}")
+    public Optional<Class> findByClassId(@PathVariable Long classId){
+        return classService.findByClassId(classId);
+    }
+
+    @DeleteMapping("class/delete/{classId}")
+    public void deleteClassBy(@PathVariable Long classId){
+        classService.deleteClassBy(classId);
+    }
 }
