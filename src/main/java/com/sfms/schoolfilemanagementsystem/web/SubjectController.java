8d3de0813e26a73183ed4a313d7c30b4c0f35b93
Oluwatoni Adeniyi <toniadeniyi14@gmail.com>
2021-10-17 16:03:10 +0100

    git commit -m "SFMS"

--- src/main/java/com/sfms/schoolfilemanagementsystem/web/SubjectController.java
+++ src/main/java/com/sfms/schoolfilemanagementsystem/web/SubjectController.java
@@ -1,7 +1,11 @@
 package com.sfms.schoolfilemanagementsystem.web;
 
+import com.sfms.schoolfilemanagementsystem.model.Subject;
+import com.sfms.schoolfilemanagementsystem.services.SubjectRegistrationDto;
 import com.sfms.schoolfilemanagementsystem.services.SubjectServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +13,11 @@ public class SubjectController {
 
     @Autowired
     private SubjectServices subjectServices;
+
+    @PostMapping("subject/add")
+    public Subject registerWith(@RequestBody SubjectRegistrationDto subjectRegistrationDto){
+        return subjectServices.registerWith(subjectRegistrationDto);
+    }
+
 
 }
